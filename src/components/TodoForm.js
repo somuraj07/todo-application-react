b103097cff255c93b8a8a0bac8cad86d878c0635
somuraj07 <somasankar33@gmail.com>
2024-12-01 13:52:07 +0530

    added todos

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,7 +1,18 @@
-import React from 'react'
+import React,{useState} from 'react'
 
-export const TodoForm = () => {
+export const TodoForm = ({addTodo}) => {
+  const [value,setValue] = useState("")
+  const handlesubmit = e => {
+    e.preventDefault();
+    addTodo(value)
+    setValue("")
+  }
   return (
-    <div>TodoForm</div>
+    <form className='TodoForm' onSubmit={handlesubmit}>
+      <input type='text' className='todo-input' value={value} placeholder='What is the task the today'
+      onChange={(e)=>setValue(e.target.value)} />
+      <button type='submit' className='todo-btn'>ADD TASK</button>
+      
+    </form>
   )
 }
--- src/components/TodoWrapper.js
+++ src/components/TodoWrapper.js
@@ -1,7 +1,25 @@
-import React from 'react'
-
+import React,{useState} from 'react'
+import { TodoForm } from './TodoForm'
+import { v4 as uuidv4 } from 'uuid';
+import { Todo } from './Todo';
+uuidv4();
 export const TodoWrapper = () => {
+  const [todos,setTodes] = useState([])
+  const addTodo = todo => {
+    setTodes([...todos,{id:uuidv4(),task:todo,completed:false,isEditing:false}])
+    console.log(todos)
+  }
+  const togglecompleted = id => {
+setTodes(todos.map(todo => todo.id === id ? {...todo,completed:!todo.completed} :todo))
+  }
   return (
-    <div>TodoWrapper</div>
+    <div className='TodoWrapper'>
+      <h1>Get Things Done !</h1>
+    < TodoForm addTodo={addTodo}/>
+    
+    {todos.map((todo,index)=> (
+      <Todo task={todo} key={index}togglecompleted={togglecompleted} />
+      ))}
+    </div>
   )
 }
